6d62fae865624cd42c4cf746e1b48c78f0d9b07f
Kevin Lahoda <klahoda@gmail.com>
2025-01-08 12:43:42 -0500

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-ss
+<h1>Welcome to Allentoen Breathes</h1>
+<p>Site coming soon.</p>
